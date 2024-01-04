faa2ff24ebb7162d23dbb459f44bbe55fe3291fb
AdityaBoghara <bogharaaditya@gmail.com>
2024-01-04 14:21:01 -0700

    Refactroring Code with Computer properties

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,23 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Aditya Boghara on 1/4/24.
+//  Copyright © 2024 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorLogic {
+    
+    
+    func CalculatorLogic(symbol: String) {
+        
+        
+        
+        
+    }
+    
+    
+    
+}
